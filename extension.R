59844484a703011f6b9ed8adce4423d77a4767fb
Daniel Hernandez <dh.danny@hotmail.com>
2019-12-12 19:55:10 +0900

    implement logistic model with genmatch results

--- extension.R
+++ extension.R
@@ -6,31 +6,45 @@ load("data_matching_not_home.Rda")
 
 # analysis for away
 
+# pre-process data for genmatch
 df_not_away <- data_matching_not_away
 df_not_away$match_place_num <- rep(0, nrow(data_matching_not_away))
 df_not_away[which(data_matching_not_away$match_place_num == 3),]$match_place_num <- 1
 df_not_away$sport <- as.numeric(df_not_away$sport)
 df_not_away$competition_dummy_factor <- as.numeric(df_not_away$competition_dummy_factor)
 
+# define treatment and the x matrix for genmatch
 Tr_away <- df_not_away$match_place_num
 X_away <- df_not_away[, -c(1, 3)]
-
+# make balance matrix for genmatch using interaction terms
 attach(df_not_away)
-X_away <- cbind(sport, competition_dummy_factor, elo_diff_adjusted_log)
 BalanceMat_away <- cbind(sport, competition_dummy_factor, elo_diff_adjusted_log, I(elo_diff_adjusted_log^2), 
                          I(competition_dummy_factor*elo_diff_adjusted_log), I(sport*competition_dummy_factor))
 detach(df_not_away)
-
-genout_away <- GenMatch(Tr=Tr_away, X=X_away, BalanceMatrix = BalanceMat_away, estimand="ATT", pop.size = 100, max.generations = 30, caliper=0.2)
-mout_away <- Match(Tr=Tr_away, X=X_away, Weight.matrix = genout_away, caliper=0.2)
-
+# run genmatch disallowing ties
+genout_away <- GenMatch(Tr=Tr_away, 
+                        X=X_away, 
+                        BalanceMatrix = BalanceMat_away, 
+                        estimand="ATT", 
+                        pop.size = 100, 
+                        max.generations = 30, 
+                        caliper=0.2,
+                        ties = FALSE
+                        )
+# match data using genmatch weights
+mout_away <- Match(Tr=Tr_away, X=X_away, Weight.matrix = genout_away, caliper=0.2, ties = FALSE)
+# evaluate balance
 mb_data_away <- data.frame(Tr=Tr_away, X_away)
-
 mb_away <- MatchBalance(Tr ~ sport + competition_dummy_factor + elo_diff_adjusted_log + I(elo_diff_adjusted_log^2) + 
                             I(competition_dummy_factor*elo_diff_adjusted_log) + I(sport*competition_dummy_factor)
                         , data = mb_data_away, match.out = mout_away, nboots = 1000)
-
-summary(mout_away) 
+# evaluate treatment effect using a logistic regression, just like in the original
+# paper.
+df_results <- df_not_away[c(mout_away$index.control, mout_away$index.treated), ]
+model_away <- glm(match_place_num ~ result_dummy_num, data = df_results, family = "binomial")
+# results
+summary(model_away)
+confint(model_away)
 
 # Analysis for home
 
